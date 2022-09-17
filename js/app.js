f3296235e8859f9e0da3ccafe0db04708eefb3b6
rniochy <lniochy@gmail.com>
2022-09-17 09:36:39 +0100

    added css struture

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const boxColor = document.getElementById('box-color');
+const inputRed = document.getElementById('red');
+const inputBlue = document.getElementById('blue');
+const inputGreen = document.getElementById('green');
+
+const checkBoxRed = document.getElementById('');
