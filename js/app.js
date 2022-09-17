17bfef5ea89376edcee8a75406c498f7495bf6ea
rniochy <lniochy@gmail.com>
2022-09-17 09:39:03 +0100

    adding features into js file

--- js/app.js
+++ js/app.js
@@ -3,4 +3,8 @@ const inputRed = document.getElementById('red');
 const inputBlue = document.getElementById('blue');
 const inputGreen = document.getElementById('green');
 
-const checkBoxRed = document.getElementById('');
+const checkBoxRed = document.getElementById('checkbox-red');
+const checkBoxBlue = document.getElementById('checkbox-blue');
+const checkBoxGreen = document.getElementById('checkbox-green');
+
+const button = document.getElementById('button');
