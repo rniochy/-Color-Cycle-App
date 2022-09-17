13acef3efee85852d8933a5da898b2f7722356c2
rniochy <lniochy@gmail.com>
2022-09-17 13:54:25 +0100

    adding events  into js file

--- README.md
+++ README.md
@@ -15,13 +15,18 @@ an RGB color specification (e.g. `#000000` color code).
 -   [ ] User can specify a starting fill color as a six hexadecimal standard
         CSS color code in three individual components of two digits each - red,
         blue, and green
+
 -   [ ] User can specify an increment value for each color component that will
         be added to that component every .25 second
+
 -   [ ] User can see the box containing the fill color change every .25 seconds
+
 -   [ ] User can only change the color components and their increments when
         the app is stopped
+
 -   [ ] User can start and stop the fill operation using a button whose name
         changes to 'Start' when stopped and 'Stop' when started
+
 -   [ ] User will receive an warning if something other than hexadecimal digits
         are entered for the color components
 
--- js/app.js
+++ js/app.js
@@ -1,39 +1,68 @@
 const boxColor = document.getElementById('box-color');
-const inputRed = document.getElementById('red');
-const inputBlue = document.getElementById('blue');
-const inputGreen = document.getElementById('green');
+let inputRed = document.getElementById('red');
+let inputBlue = document.getElementById('blue');
+let inputGreen = document.getElementById('green');
 const checkBoxRed = document.getElementById('checkbox-red');
 const checkBoxBlue = document.getElementById('checkbox-blue');
 const checkBoxGreen = document.getElementById('checkbox-green');
 const button = document.getElementById('button');
 
+const colorValues = ['0','1', '2','3','4','5','6','7', '8','9','A','B','C','D','F','G','H'];
+
+
 function setBackground(){
     boxColor.style.background = `#${inputRed.value}${inputGreen.value}${inputBlue.value}`;
-} setBackground();
-
+} 
 function disableInput(input, value){
     input.setAttribute('disabled', value);
 }
-
 function ableInput(input){
     input.removeAttribute('disabled');
 }
+function incrementColor(input){
+     let value0 = colorValues.findIndex(v=> v ===input.value[0]);
+     let value1 = colorValues.findIndex(v=> v ===input.value[1]);
+     console.log(input.value)
+     if(value0 === 16) value0 = 0; 
+     if(value1 === 16) value1 = 0;
+     
+     setInterval(()=> {
+        console.log(input.value)
+        input.value = colorValues[value0++] + colorValues[value1++];
+        setBackground();
+
+        if(value0 === 16) value0 = 0; 
+        if(value1 === 16) value1 = 0;
+        console.log(input.value)
+     }, 1000);
+}
 
 function buttonClick () {
+    
     if(button.innerText === 'Start'){
         button.innerText = "Stop";
         disableInput(inputBlue, true);
         disableInput(inputGreen, true);
         disableInput(inputRed, true);
+        disableInput(checkBoxRed, true);
+        disableInput(checkBoxBlue, true);
+        disableInput(checkBoxGreen, true);
+
     } else {
         button.innerText = "Start";
         ableInput(inputBlue);
         ableInput(inputRed);
         ableInput(inputGreen);
+        ableInput(checkBoxRed);
+        ableInput(checkBoxBlue);
+        ableInput(checkBoxGreen);
     }
 }
+
 
 button.addEventListener('click', buttonClick);
 inputRed.addEventListener('change', setBackground);
 inputGreen.addEventListener('change',setBackground);
 inputBlue.addEventListener('change',setBackground);
+setBackground();
+checkBoxBlue.addEventListener('select', ()=>{ incrementColor(inputRed)});
