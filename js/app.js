af11151920a96aabd4c1091e72990c1cc76435c5
rniochy <lniochy@gmail.com>
2022-09-18 02:03:32 +0100

    js features added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ const checkBoxRed = document.getElementById('checkbox-red');
 const checkBoxBlue = document.getElementById('checkbox-blue');
 const checkBoxGreen = document.getElementById('checkbox-green');
 const button = document.getElementById('button');
-let timer;
+
+let timer = [undefined, undefined, undefined];
+const timerBlue = 1, timerGreen = 0, timerRed= 2;
 const colorValues = ['0','1', '2','3','4','5','6','7', '8','9','A','B','C','D','F','G','H'];
 
 
@@ -20,24 +22,24 @@ function ableInput(input){
     input.removeAttribute('disabled');
 }
 
-function incrementColor(input){
-         let value0 = colorValues.findIndex(v=> v ===input.value[0].toUpperCase());
-         let value1 = colorValues.findIndex(v=> v ===input.value[1].toUpperCase());
-         
-         if(value0 === 16) value0 = 0;  if(value1 === 16) value1 = 0;
-
-            timer = setInterval(()=> {
-               input.value = colorValues[value0] + colorValues[value1];
-               setBackground();
-               if(value1 === 16){
-                   value1 = 0;
-                   value0++;
-               } else if(value0 ===16){
-                     value0 = 0;
-               } else {
-                   value1++;
-               }
-            }, 250);
+function incrementColor(input, index){
+    let value0 = colorValues.findIndex(v=> v ===input.value[0].toUpperCase());
+    let value1 = colorValues.findIndex(v=> v ===input.value[1].toUpperCase());
+    
+    if(value0 === 16) value0 = 0;  if(value1 === 16) value1 = 0;
+   
+    timer[index] =  setInterval(()=> {
+          input.value = colorValues[value0] + colorValues[value1];
+          setBackground();
+          if(value1 === 16){
+              value1 = 0;
+              value0++;
+          } else if(value0 ===16){
+                value0 = 0;
+          } else {
+              value1++;
+          }
+       }, 250);
 }
 
 function buttonClick () {
@@ -68,13 +70,13 @@ inputGreen.addEventListener('change',setBackground);
 inputBlue.addEventListener('change',setBackground);
 setBackground();
 checkBoxBlue.addEventListener('change', function(){   
-    this.checked ? incrementColor(inputBlue) : clearInterval(timer);
+    this.checked?  incrementColor(inputBlue, timerBlue) : clearInterval(timer[timerBlue])
 });
 checkBoxGreen.addEventListener('change', function(){   
-    this.checked ? incrementColor(inputGreen) : clearInterval(timer);
-});
+    this.checked?  incrementColor(inputGreen, timerGreen) : clearInterval(timer[timerGreen])
 
+});
 checkBoxRed.addEventListener('change', function(){   
-        this.checked ? incrementColor(inputRed) : clearInterval(timer);
-});
+    this.checked?  incrementColor(inputRed, timerRed) : clearInterval(timer[timerRed])
 
+})
